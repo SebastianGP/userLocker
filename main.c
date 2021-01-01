fb93bf4b64afa450c6d3f5a59a0187f731c6d653
Sebastian <43896791+SebastianGP@users.noreply.github.com>
2021-01-01 14:10:16 -0500

    Readability improved

--- main.c
+++ main.c
@@ -1,38 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <string.h>
-
-#define MAX_SZ 40
-
+#include "master.h"
 int main(void){
-    char* userName = (char*)calloc(MAX_SZ, sizeof(char));
-    printf("Enter password: ");
-    scanf("%s", userName);
+    int newUser;
     int exitStatus;
     int pipes[2];
+    char* userName = (char*)calloc(MAX_SZ, sizeof(char));
     pipe(pipes);
-    int fp = open("./locker.txt", O_WRONLY | O_CREAT, 0777);
-    pid_t pid = fork();
-    if(pid == 0){
-        close(pipes[0]);
-        close(fp);
-        write(pipes[1], userName, strlen(userName));
-        close(pipes[1]);
-    }
-    else{
-        close(pipes[1]);
-        close(0);
-        dup(pipes[0]);
-        close(1);
-        dup(fp);
-        execl("/usr/bin/shasum", "/usr/bin/shasum", NULL); 
-        close(pipes[0]);
-        close(fp);
-        pid_t deadChild = wait(&exitStatus);
+    printf("Are you new?: ");
+    scanf("%d", &newUser); 
+    if(newUser){
+        printf("Enter a password: ");
+        scanf("%s", userName);
+        int fp = open("./locker.txt", O_WRONLY | O_CREAT, 0777);
+        pid_t pid = fork();
+        firstTime(pid, pipes, userName, fp, exitStatus);
+    }else{
+        printf("Leave, implementation coming soon :)) \n");
     }
     return 0;
 }
