f6b3d9c3e3e350acefacd5fd911c775510477e63
Sebastian <43896791+SebastianGP@users.noreply.github.com>
2021-01-03 23:11:15 -0500

    user validation
    It works correctly now :)

--- main.c
+++ main.c
@@ -1,42 +1,55 @@
 #include "master.h"
-int main(void){
+
+void hashedMasterPass(char *hashedMaster);
+
+int main(void)
+{
     int newUser;
     int exitStatus;
+
+    int fp;
+
+    /* PIPES */
     int regPipes[2];
     int vetPipes[2];
-    int fp;
-    int tempFP;
+    int buffPipes[2];
+    /*  ----   */
+
+    /* BUFFERS */
     char* masterPass = (char*)calloc(MAX_SZ, sizeof(char));
-    char* tempBuffer = (char*)calloc(MAX_SZ, sizeof(char));
+    char* hashedMaster = (char*)calloc(MAX_SZ, sizeof(char));
+    char* hashedInput = (char*)calloc(MAX_SZ, sizeof(char));
+    /*  ----   */
+
+
     printf("Are you new?: ");
-    scanf("%d", &newUser); 
+    scanf("%d", &newUser);
     if(newUser){
         printf("Enter a Master password (You can't reset this): ");
         scanf("%s", masterPass);
         fp = open("./locker.txt", O_WRONLY | O_CREAT, 0777);
         pipe(regPipes);
-        pid_t pid = fork();
-        setup(pid, regPipes, masterPass, fp, exitStatus);
-        
-        printf("Setup done\n");	
-        //Portal entry is done
+        setup(regPipes, masterPass, fp, exitStatus);        
+        printf("Setup done\n");
         /*TODO: Password entries open*/
     }else{
-    	//char* tempBuffer = (char*)calloc(MAX_SZ, sizeof(char));
         printf("Enter Master password: ");
         scanf("%s", masterPass);
         pipe(vetPipes);
-        //***************TEMP FILE***************************************
-        tempFP = open("./tmp.txt", O_WRONLY | O_CREAT, 0777);
-        //***************************************************************
-        pid_t pid = fork();
-        validator(pid, vetPipes, masterPass, tempFP, tempBuffer, exitStatus);
-        //strVald(buffer, masterPass);
+        pipe(buffPipes);
+        validator(vetPipes, buffPipes, masterPass, hashedInput, exitStatus);
+        hashedMasterPass(hashedMaster);
+        strVald(hashedInput, hashedMaster);
+        /*TODO: Password entries open*/
     }
-    
-    printf("String: %s\n", tempBuffer);
-    
-    
+    freeBuffs(hashedInput, masterPass, hashedMaster);
     return 0;
-    
+}
+
+void hashedMasterPass(char *hashedMaster)
+{
+    FILE *fd;
+    fd = fopen("locker.txt", "r");
+    int x = fread(hashedMaster, 1, 40, fd);
+    fclose(fd);
 }
--- master.h
+++ master.h
@@ -12,7 +12,8 @@
 
 #define MAX_SZ 40
 
-void setup(pid_t pid, int pipes[], char* masterPass, int fp, int exitStatus){
+void setup(int pipes[], char* masterPass, int fp, int exitStatus){
+    pid_t pid = fork();
     if(pid == 0){
         close(pipes[0]);
         close(fp);
@@ -20,8 +21,10 @@ void setup(pid_t pid, int pipes[], char* masterPass, int fp, int exitStatus){
         close(pipes[1]);
     }
     else{
+        //STD_IN -> PIPE[0] -> execl(STD_IN)
         close(0);
         dup(pipes[0]);
+        //execl(STD_IN) -> STD_OUT -> FP (OPEN FILE)
         close(1);
         dup(fp); 
         close(pipes[1]);
@@ -32,40 +35,60 @@ void setup(pid_t pid, int pipes[], char* masterPass, int fp, int exitStatus){
     }
 }
 //Finish this function below for a returning user
-void strVald(char* hashedInput, char* masterPass){
-    if(strcmp(hashedInput, masterPass) == 0){
+void strVald(char* hashedInput, char* hashedMaster){
+    if(strcmp(hashedInput, hashedMaster) == 0){
         //User is in
-        printf("User is in\n");
-    }
-    else{
-        printf("Master password is incorrect. Please try again");
+        printf("Access granted.\n");
+    }else{
+        printf("Wrong\n");
     }
 }
 
-void validator(pid_t pid, int pipes[], char* masterPass, int tempFP, char* tempBuffer, int exitStatus){
+void validator(int pipes[], int buffPipes[], char* masterPass, char* hashedInput, int exitStatus){
+    pid_t pid = fork();
 	if(pid == 0){
-		//Write into pipe
 		close(pipes[0]);
-		close(tempFP);
+        close(buffPipes[0]);
+        close(buffPipes[1]);
 		write(pipes[1], masterPass, strlen(masterPass));
 		close(pipes[1]);
+        //child 2 process done
+        exit(0);
 	}
 	else{
-		//Read from pipes
-		close(0);
-		dup(pipes[0]);
-		close(1);
-		dup(tempFP);
-		close(pipes[1]);
-		execl("/usr/bin/shasum", "/usr/bin/shasum", NULL);	
-		close(pipes[0]);
-		tempBuffer[0] = getc(tempFP);
-		int bytes = read(tempFP, tempBuffer, 20);
-		close(tempFP);
-	}
-	pid_t deadChild = wait(&exitStatus);
+        pid_t pid2 = fork();
+        if(pid2 == 0){
+            //STD_IN -> PIPE[0] -> execl(STD_IN)
+            close(0);
+            dup(pipes[0]);
+            //execl(STD_IN) -> STD_OUT -> buffPipes[1]
+            close(1);
+            dup(buffPipes[1]);
+            close(pipes[1]);
+            close(buffPipes[0]);
+            execl("/usr/bin/shasum", "/usr/bin/shasum", NULL);
+            close(buffPipes[1]);
+            close(pipes[0]);
+            //Child 1 process done
+            exit(0);
+        }
+        else{
+            close(pipes[0]);
+            close(pipes[1]);
+            close(buffPipes[1]);
+            read(buffPipes[0], hashedInput, 40);
+            close(buffPipes[0]);
+            pid_t deadChild = wait(&exitStatus);
+            //Parent waits for children processes to finish
+        }
+    }
 }
 
 
+void freeBuffs(char* hashedInput, char* masterPass, char* hashedMaster){
+    free(hashedInput);
+    free(masterPass);
+    free(hashedMaster);
+}
 
 #endif
